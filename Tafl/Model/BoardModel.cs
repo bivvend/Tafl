b242ade841afefd156c557d34c40395914dc7915
bivvend <simon.j.henley@gmail.com>
2017-09-11 08:31:46 +0100

    BoardModel started

---
+++ Tafl/Model/BoardModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace Tafl.Model
+{
+    public class BoardModel
+    {
+    }
+
+    public class Board : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private int sizeX;
+        public int SizeX
+        {
+            get
+            {
+                return sizeX;
+            }
+            set
+            {
+                if(sizeX != value)
+                {
+                    sizeX = value;
+                    RaisePropertyChanged("SizeX");
+                }
+            }
+        }
+
+        private int sizeY;
+        public int SizeY
+        {
+            get
+            {
+                return sizeY;
+            }
+            set
+            {
+                if (sizeY != value)
+                {
+                    sizeY = value;
+                    RaisePropertyChanged("SizeY");
+                }
+            }
+        }
+
+        private void RaisePropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+    }
+}
