57a798ed265ffb6a86e3f42507c8b97bb5dec051
bivvend <simon.j.henley@gmail.com>
2017-11-23 08:16:13 +0000

    starting AI setup

---
+++ Tafl/AI/Move.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tafl.AI
+{
+    public class Move
+    {
+        public int startColumn { get; set; }
+        public int startRow { get; set; }
+        public int endColumn { get; set; }
+        public int endRow { get; set; }
+
+        public Move()
+        {
+            this.startColumn = 0;
+            this.startRow = 0;
+            this.endColumn = 0;
+            this.endRow = 0;
+        }
+
+        public Move(int iStartColumn, int iStartRow, int iEndColumn, int iEndRow)
+        {
+            this.startColumn = iStartColumn;
+            this.startRow = iStartRow;
+            this.endColumn = iEndColumn;
+            this.endRow = iEndRow;
+        }
+
+        public override string ToString()
+        {
+            return this.startColumn.ToString() + "," + this.startRow.ToString() + " To " + this.endColumn.ToString() + "," + this.endRow.ToString();
+        }
+    }
+}
